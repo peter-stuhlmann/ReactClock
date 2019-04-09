0cbe1e6a6f71836343a5bd6a094a3bf19a576ba0
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-09 16:48:09 +0200

    Splitted output into two functions

--- src/components/Tick.js
+++ src/components/Tick.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(
-      element,
-      document.getElementById('time')
-    );
-  }
-  
-  setInterval(tick, 1000);
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('time')
+  );
+}
+
+setInterval(tick, 1000);
