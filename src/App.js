4b5f617565313067e50499c7472933e17d109f71
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-09 12:18:56 +0200

    Added code for displaying time with interval

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Tick from './Tick';
 import './App.css';
 
 class App extends Component {
@@ -8,17 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div id="time"></div>
         </header>
       </div>
     );
