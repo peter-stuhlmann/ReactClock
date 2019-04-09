b9777dea0ac2571efe5559d3c126e2d329b6ec40
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-09 20:48:28 +0200

    Transform function to class

--- src/components/Tick.js
+++ src/components/Tick.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+class Clock extends React.Component {
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 function tick() {
   ReactDOM.render(
-    <Clock date={new Date()} />,
+    <Clock />,
     document.getElementById('time')
   );
 }
