738e6e6e91be55d033a16aae6e6819831887b4d3
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-09 12:29:47 +0200

    Updated imported files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../assets/img/logo.svg';
-import Tick from './Tick';
+import './Tick';
 import '../assets/scss/App.scss';
 
 class App extends Component {
